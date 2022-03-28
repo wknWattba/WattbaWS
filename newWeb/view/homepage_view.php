5308fc28a9aa5d05f296ff5ca55d47d3428c477b
wknWattba <102545237+wknWattba@users.noreply.github.com>
2022-03-28 19:08:27 +0100

    New Website folder

---
+++ newWeb/view/homepage_view.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <link rel="icon" href="../resources/favicon.png">
+        <link rel="stylesheet" href="../style/homepage.css?v=1.88">
+        <title> Wattba - Home </title>
+    </head>
+    
+    <div class = "navi">
+        <div class ="logo">
+            <img src = "../resources/wattbaWhite.png" alt="White Wattba Logo" width = "175" />
+        </div>
+        <nav>
+            <ul>
+                <li class ="active" onclick = "location.href='../controller/home.php';">Home</li>
+                <li onclick = "location.href='';">About Us</li>
+                <li onclick = "location.href='';">Features</li>
+                <li onclick = "location.href='';">Contact</li>
+                <li onclick = "location.href='';">Blog</li>
+            </ul>
+        </nav>
+    </div>
+
+    <body>
+        <div class ="heroImg">
+            <div class="heroText">
+                <h1> WELCOME TO WATTBA </h1>
+                <p> Welcome to WATTBA. We deliver a cutting edge booking system for Barbers,
+                    Beauticians and Salons to utilise for their business. Providing a platform for you to boost
+                    your business in the current age of technology and help improve customer acquisition. </p>
+                <br>
+                <button>Read More</button>
+            </div>
+        </div>
+        <br>
+
+        <div class ="secondTitle"> 
+            <h1> CHECK OUT THE WATTBA APP </h1>
+            <p>Download our app now! It is available on the iOS and Play Store. Our Interactive, clearly-laid-out
+                and user-friendly app seamlessly brings beauticians & Hairstylists with customers together in a single platform.
+                Customers gain access and directions to their area's unique and creative style who have customized their business
+                profile to draw in those who need their services the most, all in a few screen swipes!
+            </p>
+        </div>
+
+        <div class="container"> 
+            <div class="hero2">
+                <img src=../resources/oldtownPhone.png>
+                <br>
+                <button> DOWNLOAD NOW </button>
+            </div>
+            <div class="hero2Button">
+            </div>
+            
+            <div class="header1">
+                <img src="../resources/ftc.png">
+                <li> Find the best Barbershops & Salons in your area </li>
+                <li> 24/7 booking at your own convenience </li>
+                <li> Take your pick based on reviews, services and availability </li>
+                <li> Book as many services as you need </li>
+                <li> Pick your own stylist </li>
+                <li> Smart and Stress-Free payments </li>
+                <li> Edit or cancel up to 2 hours before your booking </li>
+                <li> Get notifications for your appointments </li>
+            </div>
+            <div class="header2">
+                <img src="../resources/ftb.png">
+                <li> Manage your schedule and appointments in one place </li>
+                <li> Edit appointments within 24 hours of booking </li>
+                <li> Showcase multiple services and styles </li>
+                <li> List one or more stylists </li>
+                <li> Create and display fully customisable promotions </li>
+                <li> Automatic email reminders to customers </li>
+            </div>
+            
+        </div>
+
+    </body>
+    
+
+</html>
