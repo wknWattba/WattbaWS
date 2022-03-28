33cda353ec2b47bccb5fe5541c6cfcbe1b5b1e1a
wknWattba <102545237+wknWattba@users.noreply.github.com>
2022-03-28 19:07:09 +0100

    Components Folder

---
+++ components/footer.php
@@ -0,0 +1,81 @@
+<div class="container">
+    <h1 class="text-center font-900 font-poppins-sami font-46 mt-5 pt-5 color-red">Don't forget to connect with us.</h1>
+    <h1 class="text-center font-900 font-poppins-sami mb-4 font-46 color-red">We'd love to stay in touch!</h1>
+    <div class="row text-center pb-5 my-5">
+        <div class="col-md-4 col-lg-4 col-4 border-right">
+            <a href="https://www.facebook.com/WATTBAAPP/"><i class="fab py-5 font-mob-i fa-facebook-f fa-7x"></i></a>
+        </div>
+        <div class="col-md-4 col-lg-4 col-4 border-right">
+            <a href="https://twitter.com/WATTBA_App"><i class="fab py-5 font-mob-i fa-twitter fa-8x"></i></a>
+        </div>
+        <div class="col-md-4 col-lg-4 col-4 border-right">
+            <a href="https://www.instagram.com/wattba_app/"><i class="fab py-5 font-mob-i fa-instagram fa-8x"></i></a>
+        </div>
+        <!--! <div class="col-md-4 col-lg-4 col-4 border-right">
+            <a href="https://www.linkedin.com/company/wattba/about/"><i class="fab py-5 font-mob-i fa-linkedin fa-8x"></i></a>
+        </div> -->
+    </div>
+</div>
+<!-- footer start -->
+<footer class="footer-bg">
+    <div class="container py-5">
+        <div class="row py-4">
+            <div class="col-lg-3">
+                <a href="/"><img src="../assets/images/logo1.png?version=<?=time();?>" class="img-size-170" width="230px"></a>
+
+                <p class="mt-5 mb-2">Email us at <a href="mailto:info@wattba.app?Subject=WATTBA Enquiry" target="_top" class="mt-5">info@wattba.app</a></p>
+                
+               <!-- <p class=" mb-2">Phone: <a href="te:03035501602">03035501602</a></p>
+                <p class=" mb-2">Skype: <a href="/">whattba</a></p> -->
+            </div>
+            <div class="col-lg-3 tab-center pl-5  pl-tab-0">
+                <div class="pl-5 tab-pt pl-tab-0">
+                    <h6>Company</h6>
+                    <ul class="navbar-nav nav mb-4 mt-2">
+                        <li class="pt-2"><a href="/">About Us</a></li>
+                        <li class="pt-2"><a href="features">Features</a></li>
+                      <!--  <li class="pt-2"><a href="">Portfolio</a></li>
+                        <li class="pt-2"><a href="">Blog</a></li> -->
+                        <li class="pt-2"><a href="sign-up">Contact</a></li>
+                        <li class="pt-2"><a href="../controller/siteadmin.php">Site Admin</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3 tab-center">
+               <h6>Services</h6>
+                <ul class="navbar-nav mb-4 nav mt-2">
+                    <li class="pt-2"><a href="../TAndC">Terms & Conditions</a></li>
+                    <li class="pt-2"><a href="../Privacy">Privacy Policy</a></li>
+                    <li class="pt-2"><a href="../Cookie">Cookie Policy</a></li>
+                    <li class="pt-2"><a href="../blog/whyonlinebooking">Blogs</a></li>
+                    <!--<li class="pt-2"><a href="">Brand analysis</a></li> -->
+                </ul>
+            </div>
+           <!-- <div class="col-lg-3 tab-center">
+                <h6>Follow Us</h6>
+                <div class="social-icons mt-3">
+                    <a href="#"><i class="fab px-0 fa-1x fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-1x fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-1x fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-1x fa-pinterest-p"></i></a>
+                    <a href="#"><i class="fab fa-1x fa-linkedin-in"></i></a>
+                </div>
+-->
+            </div>
+        </div>
+    </div>
+</footer>
+<footer class="footer-copyright">
+    <div class="container pt-4 pb-2">
+        <!-- <div class="row">
+            <div class="col-lg-6 col-md-6 col-12">
+                <p>Developed & designed by <a href="http://kamran.techgater.com/" target="_blank">Kamran Haider</a></p>
+            </div> -->
+            <div class="col-lg-6 col-md-6 text-right col-12">
+                <p>© 2019 - 2020 wattba.com | All rights reserved.</p>
+            </div>
+
+        </div>
+    </div>
+</footer>
+<!-- footer end -->
