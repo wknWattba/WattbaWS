da9a9b9af1b4175e259f23bef274caa7a1480dea
wknWattba <102545237+wknWattba@users.noreply.github.com>
2022-03-28 19:06:56 +0100

    Blog Folder

---
+++ blog/whyonlinebooking.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+   <head>
+       <link rel="apple-touch-icon" sizes="180x180" href="../assets/images/apple-touch-icon.png">
+    <link rel="icon" type="/image/png" sizes="32x32" href="../assets/images/favicon-32x32.png">
+    <link rel="icon" type="/image/png" sizes="16x16" href="../assets/images/favicon-16x16.png">
+    <link rel="manifest" href="../wattba.app">
+      <title>WATTBA - Blog</title>
+      <meta charset="UTF-8">
+      <meta name="description" content="Digitalising your Haircut Booking experience">
+      <meta name="keywords" content="HTML,CSS,Bootstrap4,XML,JavaScript">
+      <meta name="author" content="Kamran Ansari">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- fontawesome 5 CDN -->
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css?version=<?=time();?>">
+      <!-- bootstrap version 4 Link -->
+      <link rel="stylesheet" href="../assets/css/bootstrap.min.css?version=<?=time();?>">
+      <link rel="stylesheet" type="text/css" href="../assets/css/styles.css?version=<?=time();?>">
+
+      <!-- jQuery library -->
+      <script src="../assets/js/jquery.js?version=<?=time();?>"></script>
+
+      <!-- Latest compiled JavaScript -->
+      <script src="../assets/js/bootstrap.min.js?version=<?=time();?>"></script>
+   </head>
+   <body>
+      <!-- Navbar Top start -->
+      <nav class="navbar navbar-top navbar-expand-lg navbar-light py-1">
+         <div class="container">
+           <!-- <span class="text-uppercase disp-tab-none1  text-color-grey font-poppins font-13"><i class="fas font-20 trans-3y fa-phone-alt mr-3"></i>call us an make an Appointment: </span>
+            <a class="color-red font-weight-bold ml-2 font-poppins mr-auto font-13" href="tel:+928640556558"> +92 86 40 556 558</a> -->
+            <ul class="navbar-nav disp-con my-2 ml-auto my-lg-0 text-color-grey mr-3">
+               <li class="nav-item ">
+                  <a class="nav-link " href="https://www.facebook.com/WATTBAAPP"><i class="fab fa-facebook-f"></i></a>
+               </li>
+               <li class="nav-item ">
+                  <a class="nav-link" href="https://twitter.com/WATTBA_App"><i class="fab fa-twitter"></i></a>
+               </li>
+               <li class="nav-item ">
+                  <a class="nav-link" href="https://www.instagram.com/wattba_app/"><i class="fab fa-instagram"></i></a>
+               </li>
+               <!--<li class="nav-item ">
+                  <a class="nav-link" href="#"><i class="fab fa-pinterest-p"></i></a>
+               </li> -->
+               <li class="nav-item ">
+                  <a class="nav-link" href="https://www.linkedin.com/company/wattba/"><i class="fab fa-linkedin-in"></i></a>
+               </li>
+            </ul>
+            <form class="form-inline form-mob my-2 my-lg-0">
+   <button class="form-control btn input-nav input-width bor-rad bg-transparent bor-col-grey mr-sm-2"><a href="sign-up" class="text-uppercase">list your business</a></button> 
+</form>
+         </div>
+      </nav>
+      <!-- Navbar top end -->
+      <!-- hero section started -->
+      <div class="hero-bg">
+         <nav class="navbar navbar-expand-lg navbar-light pt-3">
+            <div class="container">
+               <a class="navbar-brand" href="/"><img src="../assets/images/logo1.png?version=<?=time();?>" class="navbar-brand-width" width="250px"></a>
+               <button class="navbar-toggler btn" data-toggle="collapse" data-target="#myNavbar" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+               <i class="fas color-white fa-2x fa-align-justify"></i>
+               </button>
+               <div class="collapse  navbar-collapse" id="myNavbar">
+                  <ul class="navbar-nav ml-auto  nav-hover">
+                     
+                     <li class="nav-item">
+                        <a class="nav-link px-4 mx-2 text-color-grey font-poppins font-14 text-uppercase font-weight-bold" href="/">About us</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link px-4 mx-2 text-color-grey font-poppins font-14 text-uppercase font-weight-bold" href="features">Features</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link px-4 mx-2 text-color-grey font-poppins font-14 text-uppercase font-weight-bold" href="pricing">pricing</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link px-4 mx-2 text-color-grey font-poppins font-14 text-uppercase font-weight-bold" href="sign-up">contact us</a>
+                     </li>
+                  </ul>
+               </div>
+            </div>
+         </nav>
+         <div class="container mb-5   py-5">
+            <div class="row mb-5 p0-0 py-5">
+               <div class="col-lg-6 text-mobile col-md-6 col-sm-12 my-5 pr-0">
+                  <img src="../assets/images/Blog-OnlineBooking.png?version=<?=time();?>" class="img-fluid mt-5 mb-mob-h">
+                  <button class="btn btn-transparent pt-btn text-uppercase bg-transparent p0-0 bor-rad px-4 font-13 bor-1-white mt-5"><a href="sign-up" class="color-white dec-none">Get in touch</a></button>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- barbar shop section started -->
+      <div class="container py-5 mt-5">
+         <!--<div class="row mt-5  mar-top-0">
+            <div class="">
+               <button class="btn btn-secondary bor-rad text-uppercase px-4 font-13 mt-4 font-poppins pt-2 font-weight-bold"><a href="sign-up" class="color-white dec-none">Get in touch!</button> -->
+               <h1 style="text-align: center;"><strong>WHY YOU SHOULD LEVERAGE ONLINE BOOKING SYSTEMS FOR YOUR BARBER SHOP</strong></h1>
+<p><span style="font-weight: 400;">As an enterprising hairstylist, you&rsquo;re surely thinking around the clock about ways to make your barber shop unique and more efficient. If you&rsquo;ve thought of integrating online booking, then you&rsquo;re definitely looking in the right direction.&nbsp;</span></p>
+<p><span style="font-weight: 400;">Hotels, spas, resorts, transportation companies and countless other establishments have been harnessing the amazing </span><strong>benefits of online booking systems</strong><span style="font-weight: 400;"> for a long time and yielding bountiful results.</span></p>
+<p><span style="font-weight: 400;">Incorporating an </span><strong>online booking system</strong><span style="font-weight: 400;"> into your barber shop is one sure way to stand out from competitors and provide the convenience that your customers deserve, but there are so many other wonderful </span><strong>benefits of online booking systems</strong><span style="font-weight: 400;">.</span></p>
+<ul>
+<li><strong><strong>24/7 AVAILABILITY</strong></strong></li>
+</ul>
+<p><span style="font-weight: 400;">Even if you&rsquo;re closed, customers don&rsquo;t have to wait till the next day to book their barber appointments. They&rsquo;ll have real-time access to your business 24/7 at their own convenience! Just imagine how many customers must have visited your barber shop at your closing hours, met your doors closed, and turned around not sure how to schedule a favourable time.&nbsp;</span></p>
+<ul>
+<li><strong><strong>FINANCIAL COMMITMENT</strong></strong></li>
+</ul>
+<p><span style="font-weight: 400;">When customers pay to book a haircut online, they are less likely to cancel. So, an </span><strong>online booking system</strong><span style="font-weight: 400;"> will reduce the number of no-shows. Still, even if a customer does have to cancel, the slot simply opens up for another customer to fill. This is definitely one of the reasons </span><strong>why online booking is better</strong><span style="font-weight: 400;">.</span></p>
+<ul>
+<li><strong><strong>QUICK, SECURE AND CONVENIENT PAYMENT</strong></strong></li>
+</ul>
+<p><span style="font-weight: 400;">The online market is growing exponentially by the minute. A lot of customers find it far more convenient to make their reservations and payments online ahead of time. You can also choose to incentivize online booking by creating special deals for customers that book their haircuts online.</span></p>
+<ul>
+<li><strong><strong>VALUABLE DATA ANALYTICS</strong></strong></li>
+</ul>
+<p>&nbsp;<span style="font-weight: 400;">An </span><strong>online booking system</strong><span style="font-weight: 400;"> is one of the most efficient ways to collate data on the performance of your business. With the analytics of an excellent online booking platform, you can tell which hours your traffic is highest or least, who your most frequent customers are, and what styles or trends people are most interested in. And then you can make use of this information in planning your moves and creating specific offers that will excite your customers.</span></p>
+<ul>
+<li><strong><strong>TIME SAVING</strong></strong></li>
+</ul>
+<p>&nbsp;<span style="font-weight: 400;">As well as saving customers&rsquo; time, an </span><strong>online barber shop</strong><span style="font-weight: 400;"> booking system will save the precious time of your dedicated staff. You&rsquo;ll be cutting down on redundant time spent making appointments via phone calls. And the fact that this time can be reinvested in other productive activities at the shop is another pointer as to </span><strong>why online booking is better</strong><span style="font-weight: 400;">.</span></p>
+<p><span style="font-weight: 400;">As an innovative entrepreneur, you know that the best way keep your business on top at all times is to align with the trends as they come. Online booking is one trend that is here to stay because of the convenience and reliability that it affords both the business and the prospective customer. This said, you&rsquo;ll have to ensure that while you&rsquo;re setting up your </span><strong>online barber shop</strong><span style="font-weight: 400;">, you&rsquo;re also preparing your onsite barber shop for the influx of new and old customers that you&rsquo;ll be sure to get, as well as all the other inevitable </span><strong>benefits of online booking systems</strong><span style="font-weight: 400;">.</span>&nbsp;</p>
+<p>&nbsp;</p>
+<!-- Download app section -->
+      <div class="App-sec-bg">
+         <div class="container py-5">
+            <div class="row pt-3">
+               <div class="col-lg-6 col-lg-offest-6"></div>
+               <div class="col-lg-6 text-mobile">
+                  <img src="../assets/images/logo1.png?version=<?=time();?>" class="img-size-170 pb-mob" width="230px">
+                  <h1 class="text-uppercase font-mob-20 color-white font-900 pb-mob font-poppins-sami  font-46">the mobile app</h1>
+                  <p class="mb-2 font-poppins font-18 color-white font-mob-14 pb-mob">COMING SOON TO BE...</p>
+                  <img src="../assets/images/appstore.png?version=<?=time();?>" width="120px">
+                  <img src="../assets/images/googleplay.png?version=<?=time();?>" class="mt-1" width="140px">
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- Download app section end-->
+              
+            </div>
+         </div>
+      </div>
+      <!-- contact us section started -->
+      <div class="container">
+         <div class="text-center">
+            <h1 class="text-center mt-5 color-red font-900 font-poppins-sami  font-46">Contact Us</h1>
+            <p class="mb-0 font-poppins color-black font-28">Providing with few small details and</p>
+            <p class="font-poppins color-black mb-1 font-28">We'll be in touch shortly</p>
+            <img src="../assets/images/devider.jpg?version=<?=time();?>">
+         </div>
+         <form action="/" method="POST">
+            <div class="row mt-5">
+               <div class="col-md-6 col-lg-6 col-sm-12 col-12 pt-1">
+                  <input type="text" class="form-control input-form pl-4  bor-rad mb-3 py-2 " name="name" placeholder="Enter your complete name">
+                  <input type="text" class="form-control input-form pl-4  bor-rad mb-3 py-2  mt-1" name="name" placeholder="Enter your complete Phone No:">
+                  <input type="text" class="form-control input-form pl-4  bor-rad mb-3 py-2  mt-2" name="name" placeholder="Enter your complete Email address">
+                  <input type="text" class="form-control input-form pl-4  bor-rad mb-3 py-2  mt-1" name="name" placeholder="Enter your complete Business">
+               </div>
+               <div class="col-md-6 col-lg-6 col-sm-12 col-12-">
+                  <textarea class="form-control input-form pl-4 pt-4 bor-rad mb-3" rows="8" name="name" placeholder="How can we help?"></textarea>
+               </div>
+               <input type="submit" class="mx-auto bor-rad px-5 text-uppercase font-13 mt-4 btn bg-gradiant color-white" name="submit">
+            </div>
+         </form>
+      </div>
+      <!-- contact us section ended -->
+      <?php include '../components/footer.php';?>
+   </body>
+</html>
